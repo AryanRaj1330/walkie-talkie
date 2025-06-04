fa5cdc1844fe2fc5c66fa342b13716d5971cd179
AryanRaj1330 <24je0890@iitism.ac.in>
2025-06-05 01:06:24 +0530

    database setup started

---
+++ server/backend/models/userModel.jsx
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const userModel= mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true},
+    picture:{type:String,required:true,default:"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"}
+},{
+    timestamps:true
+})
+
+const user=mongoose.model("User",userModel)
+export default user
